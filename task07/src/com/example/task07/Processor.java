c755815bb30ea094c4b058210dffebff797c849a
Dmitry <hacker.dmitry@gmail.com>
2019-03-30 12:42:32 +0500

    Solving task07

--- task07/src/com/example/task07/Processor.java
+++ task07/src/com/example/task07/Processor.java
@@ -1,9 +1,15 @@
 package com.example.task07;
 
 public class Processor {
+    static int numEx = 0;
 
     public Object process() throws Exception {
-        //todo вы можете заменить реализацию этого метода для ручного дебага
+        switch (numEx++) {
+            case 0:
+                throw new Exception();
+            case 1:
+                throw new RuntimeException();
+        }
         return null;
     }
 
--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -5,20 +5,24 @@ public class Task07Main {
     public static final String CHECKED = "checked";
     public static final String UNCHECKED = "unchecked";
     public static final String NONE = "none";
-
-    public static void main(String[] args) {
-    }
-
     public Processor processor;
 
-    public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
+    public static void main(String[] args) {
+        Task07Main task07Main = new Task07Main();
+        System.out.println(task07Main.getExceptionType());
+        System.out.println(task07Main.getExceptionType());
+        System.out.println(task07Main.getExceptionType());
+    }
 
+    public String getExceptionType() {
+        try {
+            processor.process();
+        } catch (RuntimeException e) {
+            return UNCHECKED;
+        } catch (Exception e) {
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
