f7ba293f2e91eaf0c9198558e24537662ec21ca0
wisedog <psnetwork1138@gmail.com>
2019-03-30 10:52:55 +0500

    Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,12 @@
 package com.example.task03;
 
+import java.io.FileWriter;
+
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        throwCheckedException();
-         */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws Exception {
+        FileWriter wr = new FileWriter("");
     }
 }
