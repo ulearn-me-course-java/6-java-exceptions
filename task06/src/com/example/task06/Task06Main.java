88af02651e40e988ae944f34761464b35288afb7
wisedog <psnetwork1138@gmail.com>
2019-03-30 10:43:59 +0500

    Task06

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,20 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        new Task06Main().printMethodName();
-         */
+        Task06Main t = new Task06Main();
+
+        t.printMethodName();
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        StackTraceElement[] st = Thread.currentThread().getStackTrace();
 
+
+        for (int i = 0; i < st.length; i++) {
+            if (st[i].getMethodName() == "printMethodName") {
+                System.out.println(st[i+1].getMethodName());
+                break;
+            }
+        }
+    }
 }
