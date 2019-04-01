be2d90d9f3b2d148cc1c49be202f23ed143a56fd
wisedog <psnetwork1138@gmail.com>
2019-04-01 18:44:49 +0500

    Task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,12 +1,8 @@
 package com.example.task03;
 
-import java.io.FileWriter;
-
 public class Task03Main {
     public static void main(String[] args) {
     }
 
-    public static void throwCheckedException() throws Exception {
-        FileWriter wr = new FileWriter("");
-    }
+    public static void throwCheckedException() throws Exception { throw new Exception(); }
 }
