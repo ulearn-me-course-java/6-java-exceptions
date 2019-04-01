0ce86931fa5e31cc871f41a01a78cff2281549c6
wisedog <psnetwork1138@gmail.com>
2019-04-01 18:52:41 +0500

    Task06

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -3,19 +3,10 @@ package com.example.task06;
 public class Task06Main {
     public static void main(String[] args) {
         Task06Main t = new Task06Main();
-
         t.printMethodName();
     }
 
     void printMethodName() {
-        StackTraceElement[] st = Thread.currentThread().getStackTrace();
-
-
-        for (int i = 0; i < st.length; i++) {
-            if (st[i].getMethodName() == "printMethodName") {
-                System.out.println(st[i+1].getMethodName());
-                break;
-            }
-        }
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
     }
 }
