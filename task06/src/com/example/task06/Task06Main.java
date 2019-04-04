08c8b7b4e65dc6f12bfb0121693b703332289c22
Henkka <henkkawar@gmail.com>
2019-04-04 11:52:16 +0500

    add reflection

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -1,6 +1,9 @@
 package com.example.task06;
 
+import sun.reflect.Reflection;
+
 import javax.imageio.IIOException;
+import java.lang.reflect.Method;
 
 public class Task06Main {
     public static void main(String[] args) {
@@ -12,7 +15,9 @@ public class Task06Main {
     }
 
     void printMethodName() {
-        System.out.println(new Throwable().getStackTrace()[1].getMethodName());
+        //System.out.println(new Throwable().getStackTrace()[1].getMethodName());
+        String name = Reflection.getCallerClass(2).getMethods()[2].getName();
+        System.out.println(name);
     }
 
 }
