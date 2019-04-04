9a094ff0e08e193ac9c9c092fe6ef73489e9f580
AllaOsipyan <allosipyan@mail.ru>
2019-04-04 18:40:38 +0500

    Осипян Пайцар.Задание03

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -5,13 +5,22 @@ public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getSeason(-5));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber == 12 || monthNumber > 0 && monthNumber <= 2)
+            return "зима";
+        else if (monthNumber >= 3 && monthNumber <= 5)
+            return "весна";
+        else if (monthNumber >= 6 && monthNumber <= 8)
+            return "лето";
+        else if (monthNumber >= 9 && monthNumber <= 12)
+            return "осень";
+        else
+            throw new MyException("monthNumber " + monthNumber + " is invalid, month number should be between 1..12");
     }
 
 }
