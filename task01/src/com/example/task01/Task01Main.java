110cf182d182050bca34f243467e9c7d1e7097e6
shee4 <lilite71@gmail.com>
2019-04-05 03:27:32 +0500

    task

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,14 +1,16 @@
 package com.example.task01;
 
-import java.math.BigDecimal;
-
 public class Task01Main {
     public static void main(String[] args) {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
         codeWithNPE();
+         */
     }
 
     static void codeWithNPE() {
-        BigDecimal[] a = new BigDecimal[1];
-        a[0].add(BigDecimal.valueOf(1));
+        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
+
