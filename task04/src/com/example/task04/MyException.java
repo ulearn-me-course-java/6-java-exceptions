d313283f40fac5adb17beb4990a72bfcaf175635
Pavel <pavel.zaborin@inbox.ru>
2019-04-10 14:39:24 +0500

    Заборин Павел. Задание 04

---
+++ task04/src/com/example/task04/MyException.java
@@ -0,0 +1,12 @@
+package com.example.task04;
+
+public class MyException extends IllegalArgumentException{
+
+    public MyException() {
+        super();
+    }
+
+    public MyException(String s) {
+        super(s);
+    }
+}
