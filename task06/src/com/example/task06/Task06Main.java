c62e3fe8a192bb5e0107de051ea1f4839e545bee
Pavel <pavel.zaborin@inbox.ru>
2019-04-10 14:57:17 +0500

    Заборин Павел. Задание 06

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,12 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         new Task06Main().printMethodName();
-         */
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        StackTraceElement[] trace =Thread.currentThread().getStackTrace();
+        System.out.print(trace[2].getMethodName());
     }
 
 }
