67f119df19cf634c59b3e60b1a5ac464cb085e94
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-04-11 20:57:27 +0500

    Update Task07Main.java

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -12,13 +12,17 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+            processor.process();
         }
-        return null;
-    }
+        catch (RuntimeException ex){
+            return UNCHECKED;
+        }
+        catch (Exception ex) {
+            return CHECKED;
+        }
 
+        return NONE;
+    }
 }
