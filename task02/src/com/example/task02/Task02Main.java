c8f8fc0e5d8259ffd7324d92c11cf538531fa0f8
6yryndy4una <semeninoff@gmail.com>
2019-11-27 10:52:31 +0500

    [Семенинов] Задание02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -11,6 +11,37 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber >= 1 && monthNumber <= 12) {
+            switch (monthNumber) {
+                case 12:
+                    return "зима";
+                case 1:
+                    return "зима";
+                case 2:
+                    return "зима";
+                case 3:
+                    return "весна";
+                case 4:
+                    return "весна";
+                case 5:
+                    return "весна";
+                case 6:
+                    return "лето";
+                case 7:
+                    return "лето";
+                case 8:
+                    return "лето";
+                case 9:
+                    return "осень";
+                case 10:
+                    return "осень";
+                case 11:
+                    return "осень";
+                default:
+                    return "";
+            }
+        } else {
+            throw new IllegalArgumentException("monthNumber" + " " + monthNumber + " " + "is invalid, month number should be between 1..12");
+        }
     }
 }
