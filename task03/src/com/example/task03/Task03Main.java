af6031444956136c4459055c447be4656d6d7b20
LordOfTheDeadlines <vedroid28@mail.ru>
2020-03-20 20:03:18 +0500

    Сидорчук Анастасия. 6.03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,13 +1,17 @@
 package com.example.task03;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Task03Main {
     public static void main(String[] args) throws Exception {
         throwCheckedException();
     }
 
     //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() throws Exception{
-        System.out.println(":(");
-        throw new Exception();
+    public static void throwCheckedException() throws IOException {
+        FileWriter fileWriter = new FileWriter("out.txt");
+        fileWriter.close();
+        fileWriter.write("Хьюстон, у нас проблема");
     }
 }
