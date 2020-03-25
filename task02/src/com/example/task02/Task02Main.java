9efca463b1fe1dde524c9ee37957a0b85866c5a6
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-25 20:37:32 +0500

    Трушникова Елизавета - 6.2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,9 +3,7 @@ package com.example.task02;
 public class Task02Main {
 
     public static void main(String[] args) {
-
         System.out.println(getSeason(-5));
-
     }
 
     static String getSeason(int monthNumber) {
