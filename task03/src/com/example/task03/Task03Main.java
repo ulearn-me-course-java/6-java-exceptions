b6c77fa8d6fb30f98d49be1c10b80d5112ae7ad5
Ekaterina <slalam.ww@gmail.com>
2020-04-03 11:41:42 +0500

    Абрамкова, 6.03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,5 +1,9 @@
 package com.example.task03;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -9,8 +13,10 @@ public class Task03Main {
          */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws IOException {
+        FileWriter  newFileWriter = new FileWriter("C://Users//kater//Desktop//Semester.txt");
+        newFileWriter.close();
+        newFileWriter.write("Друид-мизантроп вылез из пещеры");
     }
 }
+
