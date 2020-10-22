ca4bda212b37ec40b906f659b67cb72bd0ce2bd4
Евгений <Evgeniy.vm11@gmail.com>
2020-10-22 16:33:55 +0500

    Михайловский Евгений 6.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -3,15 +3,23 @@ package com.example.task04;
 public class Task04Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getSeason(-5));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+            if (monthNumber > 12 || monthNumber < 1)
+                throw new MyException("monthNumber "+monthNumber+" is invalid, month number should be between 1..12");
+            else {
+                if (monthNumber == 1 || monthNumber == 2 || monthNumber == 12) return "зима";
+                if (monthNumber == 3 || monthNumber == 4 || monthNumber == 5) return "весна";
+                if (monthNumber == 6 || monthNumber == 7 || monthNumber == 8) return "лето";
+            }
+            return "осень";
     }
 
+}
+class MyException extends IllegalArgumentException {
+    public MyException (String messege){
+        super(messege);
+    }
 }
