e10749af0f2e1b02469a4967c04e9ca39d8f906a
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-22 21:12:52 +0500

    Ледянкин Павел. Задание 6.04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,8 +1,9 @@
 package com.example.task04;
 
-public class Task04Main {
-
-    public static void main(String[] args) {
+public class Task04Main
+{
+    public static void main(String[] args)
+    {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
@@ -10,8 +11,18 @@ public class Task04Main {
          */
     }
 
-    static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    static String getSeason(int monthNumber)
+    {
+        if (monthNumber > 12 || monthNumber < 1)
+            throw new MyException(String.format
+                    ("monthNumber %d is invalid, month number should be between 1..12", monthNumber));
+        if (monthNumber >= 3 && monthNumber < 6)
+            return "весна";
+        if (monthNumber >= 6 && monthNumber < 9)
+            return "лето";
+        if (monthNumber >= 9 && monthNumber < 12)
+            return "осень";
+        return "зима";
     }
 
 }
