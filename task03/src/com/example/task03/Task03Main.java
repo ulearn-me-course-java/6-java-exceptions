2dd2306307c279859544697950a98b822b6624f2
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-23 19:04:21 +0500

    Ледянкин Павел. Задание 6.03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,15 +1,9 @@
 package com.example.task03;
 
-import com.sun.org.omg.CORBA.ExceptionDescription;
-import jdk.jfr.events.ExceptionThrownEvent;
-
-import java.io.*;
-
 public class Task03Main
 {
     public static void main(String[] args)
     {
-        //throwCheckedException();
     }
 
     public static void throwCheckedException() throws Exception
