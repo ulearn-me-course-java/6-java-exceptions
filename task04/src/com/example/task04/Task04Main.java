e0443e1cc4d55110a905932ad9c7b2923b3df759
Sofya <fiepaz03@gmail.com>
2021-03-20 17:37:36 +0500

    Task 6.4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -11,7 +11,17 @@ public class Task04Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber <= 0 || monthNumber > 12)
+            throw new MyException(
+                    String.format("monthNumber %d is invalid, month number should be between 1..12",
+                            monthNumber));
+        if (monthNumber == 12 || monthNumber <= 2)
+            return "зима";
+        else if (monthNumber <= 5)
+            return "весна";
+        else if (monthNumber <= 8)
+            return "лето";
+        else return "осень";
     }
 
 }
