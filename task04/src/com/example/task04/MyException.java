bb20739a38d129527df9dda909a4c2a9ecbe148a
Evgeniy <djonich@mail.ru>
2021-03-25 13:31:17 +0500

    Ничипоренко Евгений. Задание 6.4

--- task04/src/com/example/task04/MyException.java
+++ task04/src/com/example/task04/MyException.java
@@ -1,7 +1,19 @@
 package com.example.task04;
 
 public class MyException extends IllegalArgumentException {
+    private String message;
+
     MyException(String message){
-        super(message);
+        setMessage(message);
+        getMessage();
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
