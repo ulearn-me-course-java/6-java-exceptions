f84b9adc253dc8d151bd36e02ce115b556dfa82c
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-04-01 14:38:43 +0500

    Revert "Task 6"
    This reverts commit 992995222ff51eab0df0c55a47c79df7c52f31b2.

--- task07/src/com/example/task07/Processor.java
+++ task07/src/com/example/task07/Processor.java
@@ -3,9 +3,8 @@ package com.example.task07;
 public class Processor {
 
     public Object process() throws Exception {
-
+        //todo вы можете заменить реализацию этого метода для ручного дебага
         return null;
-
     }
 
 }
--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -7,17 +7,18 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
-
     }
 
     public Processor processor;
 
     public String getExceptionType() {
+        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process();
-        } catch (Error | RuntimeException e) { return UNCHECKED; }
-          catch (Exception e) { return CHECKED; }
-        return NONE;
+            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+        } catch (Exception e) {
+
+        }
+        return null;
     }
 
 }
