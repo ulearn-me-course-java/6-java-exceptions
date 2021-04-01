992995222ff51eab0df0c55a47c79df7c52f31b2
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-04-01 14:35:44 +0500

    Task 6

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -7,18 +7,17 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+
     }
 
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
-        }
-        return null;
+            processor.process();
+        } catch (Error | RuntimeException e) { return UNCHECKED; }
+          catch (Exception e) { return CHECKED; }
+        return NONE;
     }
 
 }
