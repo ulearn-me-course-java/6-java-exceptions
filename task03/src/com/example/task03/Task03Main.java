0a272e571ae8214a6cf9de15209c74e1c35bb893
Anny <70793585+anyast103@users.noreply.github.com>
2022-03-02 14:15:51 +0300

    Task_6.3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,14 @@
 package com.example.task03;
 
+import java.io.*;
+
 public class Task03Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main(String[] args) throws IOException {
         throwCheckedException();
-         */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws IOException {
+        FileReader file = new FileReader("nonexistent_file.class");
+        file.read();
     }
 }
