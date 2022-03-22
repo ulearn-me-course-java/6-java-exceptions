50a042f71b6858d114406a47f8da89b22982aec8
Vlad <luckyvalenok1@gmail.com>
2022-03-22 15:22:13 +0500

    Added Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,14 +3,20 @@ package com.example.task02;
 public class Task02Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getSeason(-5));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber == 12 || (monthNumber > 0 && monthNumber < 3)) {
+            return "зима";
+        } else if (monthNumber > 2 && monthNumber < 6) {
+            return "весна";
+        } else if (monthNumber > 5 && monthNumber < 9) {
+            return "лето";
+        } else if (monthNumber > 8 && monthNumber < 12) {
+            return "осень";
+        } else {
+            throw new IllegalArgumentException("monthNumber " + monthNumber + " is invalid, month number should be between 1..12");
+        }
     }
 }
