8dd9568aec52b4b4ff737c2b584948213620385d
IlyaIIS <ilya1221221@mail.ru>
2022-03-28 20:04:58 +0500

    Исмаков Илья - task 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,16 @@
 package com.example.task03;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Task03Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    public static void main(String[] args) throws IOException{
         throwCheckedException();
-         */
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws IOException {
+        List<String> s = Files.readAllLines(Paths.get("/"));
     }
 }
