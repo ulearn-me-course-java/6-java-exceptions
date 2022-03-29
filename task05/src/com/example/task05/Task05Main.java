2e353c7f794aa156db75bcd5fc545181a929c63e
Fofan <vovka.322@Mail.ru>
2022-03-30 03:05:45 +0600

    Ефимов Иван task05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,15 +1,25 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
-        String pathToFile = args[0]; // "/home/user/file.txt"
+    public static void main(String[] args){
 
-        String s = readFile(pathToFile);
-        System.out.println(s);
+        String pathToFile = args[0];   // "/home/user/file.txt"
+
+        try{
+            String s = readFile(pathToFile);
+            System.out.println(s);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("файл \"" + pathToFile + "\" не найден");
+        }
+        catch(IOException e){
+            System.out.println("произошла ошибка при чтении файла \"" + pathToFile + "\"");
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
