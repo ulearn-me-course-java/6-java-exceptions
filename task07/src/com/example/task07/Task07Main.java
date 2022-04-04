32f0051a3acebde6bc270b7020d73d5fe66913b6
Amir <70907990+Amirka2@users.noreply.github.com>
2022-04-05 00:40:09 +0500

    Задание 07. Файзуллин Амир. При-201

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -1,5 +1,9 @@
 package com.example.task07;
 
+import jdk.jfr.internal.Logger;
+
+import java.util.logging.Level;
+
 public class Task07Main {
 
     public static final String CHECKED = "checked";
@@ -12,13 +16,14 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
             processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+        } catch (RuntimeException e) {
+            return UNCHECKED;
         } catch (Exception e) {
-
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
