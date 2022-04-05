2b08896b50c69df0afb567a28432812b3a90d6f3
sphinxan <dazaj.osamu@mail.ru>
2022-04-06 00:15:58 +0500

    Ковыляева Настя. Задание 6.6

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -4,13 +4,16 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         new Task06Main().printMethodName();
-         */
+
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        System.out.print(ste[2].getMethodName());
+
     }
 
 }
