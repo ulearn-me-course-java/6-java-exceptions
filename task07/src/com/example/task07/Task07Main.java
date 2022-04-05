f7d00380d1d41689afe59dc3df085e68c3fef1fa
vlaD <vlad.74022@gmail.com>
2022-04-06 01:07:17 +0500

    Task6.07 Рытый Владислав, ПрИ-201;

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -5,20 +5,21 @@ public class Task07Main {
     public static final String CHECKED = "checked";
     public static final String UNCHECKED = "unchecked";
     public static final String NONE = "none";
+    public Processor processor;
 
     public static void main(String[] args) {
     }
-
-    public Processor processor;
 
     public String getExceptionType() {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
             processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
+        } catch (RuntimeException e) {
+            return "unchecked";
         } catch (Exception e) {
-
+            return "checked";
         }
-        return null;
+        return "none";
     }
 
 }
