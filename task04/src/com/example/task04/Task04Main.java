1523dfcb788a7bfc5c1b5efc49c6acfc28dea162
Boris Tarelkin <tarelkinba@gmail.com>
2022-10-18 21:52:11 +0500

    Egor Gorbunov PrI-201

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -5,13 +5,24 @@ public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
+
+        System.out.println(getSeason(5));
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber < 1 || monthNumber > 12) {
+            throw new MyException(String.format("monthNumber %s is invalid, month number should be between 1..12", monthNumber));
+        }
+        int season = monthNumber % 12;
+        if (season < 3){
+            return "winter";
+        } else if (season < 6) {
+            return "spring";
+        } else if (season < 9) {
+            return "summer";
+        }
+        return "autumn";
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -3,13 +3,20 @@ package com.example.task05;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String pathToFile = args[0]; // "/home/user/file.txt"
 
-        String s = readFile(pathToFile);
-        System.out.println(s);
+        try {
+            String s = readFile(pathToFile);
+            System.out.println(s);
+        }catch (FileNotFoundException e){
+            System.out.printf("file \"%s\" not found\n", pathToFile);
+        }catch (IOException e){
+            System.out.printf("An error occurred while reading the file \"%s\"", pathToFile);
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
