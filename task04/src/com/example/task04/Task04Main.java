81a2ef8e0edaf2f1efa5a868053d24ffb5dcdfa4
Настя <nastamezenceva9155@gmail.com>
2022-10-23 21:31:41 +0500

    all

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,5 +1,7 @@
 package com.example.task04;
 
+import java.text.MessageFormat;
+
 public class Task04Main {
 
     public static void main(String[] args) {
@@ -11,7 +13,21 @@ public class Task04Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        if(monthNumber == 1 || monthNumber == 2 || monthNumber == 12){
+            return "зима";
+        } else if(monthNumber == 3 || monthNumber == 4 || monthNumber == 5){
+            return "весна";
+        } else if(monthNumber == 6 || monthNumber == 7 || monthNumber == 8){
+            return "лето";
+        } else if(monthNumber == 9|| monthNumber == 10 || monthNumber == 11){
+            return "осень";
+        } else{
+            throw new MyException(
+                    MessageFormat.format("monthNumber {0} is invalid, month number should be between 1..12",
+                            monthNumber)
+            );
+        }
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,15 +1,22 @@
 package com.example.task05;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.text.MessageFormat;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         String pathToFile = args[0]; // "/home/user/file.txt"
-
-        String s = readFile(pathToFile);
-        System.out.println(s);
+        String s = null;
+        try {
+            s = readFile(pathToFile);
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            String str = String.format("файл \"%s\" не найден\n", pathToFile);
+            System.out.printf(str);
+        } catch (IOException e){
+            String str = String.format("произошла ошибка при чтении файла \"%s\"", pathToFile);
+            System.out.printf(str);
+        }
     }
 
     public static String readFile(String pathToFile) throws IOException {
