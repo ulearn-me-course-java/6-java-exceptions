f090658f8011f3c6e071d007d99a4a009db6ed78
YesNile <holmovdanil2@gmail.com>
2022-11-05 21:57:08 +0500

    Lab6

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -7,6 +7,7 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+
     }
 
     public Processor processor;
@@ -15,10 +16,14 @@ public class Task07Main {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
             processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
 
+            return UNCHECKED;
+        }catch (Exception e)
+        {
+            return CHECKED;
         }
-        return null;
+        return NONE;
     }
 
 }
