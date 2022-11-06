015992b2d9a225af027777a347846658970cea42
Danila <Deplodoctor@gmail.com>
2022-11-06 19:45:14 +0500

    Lab6 end

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,14 +1,22 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         String pathToFile = args[0]; // "/home/user/file.txt"
+        String s = "";
+        try{
+            s = readFile(pathToFile);
+        }catch (FileNotFoundException e){
+            System.out.print("файл \"" + args[0] + "\" не найден");
+        }catch (IOException e){
+            System.out.print("роизошла ошибка при чтении файла \"" + args[0] + "\"");
+        }
 
-        String s = readFile(pathToFile);
         System.out.println(s);
     }
 
