e907af3e1d4fd07383314394fee6be7e230ac6bb
nbespalovv <Dnevnik1400@mail.ru>
2022-11-20 18:48:14 +0500

    Задание 3

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,22 @@
 package com.example.task03;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Task03Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         throwCheckedException();
-         */
+
     }
 
     //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws FileNotFoundException {
+        File file = new File("myFile");
+        FileInputStream fis = new FileInputStream(file);
+        System.out.println("Hello");
     }
 }
