e5bc1d3a1a4c62578423411d40059c198204b9a2
informant <artemiy.solodkin@mail.ru>
2023-09-24 17:28:21 +0500

    2 done

--- task01/src/com/example/task01/Tests.java
+++ task01/src/com/example/task01/Tests.java
@@ -3,7 +3,6 @@ package com.example.task01;
 import org.junit.Test;
 
 public class Tests {
-
     @Test(expected = NullPointerException.class)
     public void testCodeWithNPE() {
         Task01Main.codeWithNPE();
