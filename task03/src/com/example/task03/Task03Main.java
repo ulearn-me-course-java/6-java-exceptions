f7f9d0b031b05a0481a9002ce0d3b4f66f0cfcb5
informant <artemiy.solodkin@mail.ru>
2023-09-24 17:39:10 +0500

    4 DOne

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,16 +1,19 @@
 package com.example.task03;
 
+import java.io.IOException;
+
 public class Task03Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        throwCheckedException();
-         */
+
+        Tests tests = new Tests();
+        tests.testThrowCheckedException();
+
     }
 
     //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws IOException {
+        throw new IOException();
     }
 }
--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -3,15 +3,27 @@ package com.example.task04;
 public class Task04Main {
 
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(-5));
-         */
+
+        Tests tests = new Tests();
+        tests.testGetSeason_shouldFailWhenNegativeMonthNumber();
+        tests.testGetSeason_shouldFailWhenZeroMonthNumber();
+        tests.testGetSeason_shouldFailWhenTooBigMonthNumber();
+        tests.testGetSeason_withCorrectInput();
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber>12||monthNumber<=0){
+            throw new  MyException(String.format("monthNumber %s is invalid, month number should be between 1..12",monthNumber));
+        }
+        String res = "весна";
+        if (monthNumber==1||monthNumber==2||monthNumber==12){
+            res = "зима";
+        }else if (monthNumber>5 && monthNumber<9){
+            res = "лето";
+        } else if (monthNumber>8 && monthNumber<12){
+            res = "осень";
+        }
+        return res;
     }
 
 }
