c38839998317bd7fb5f86cad7ed49a8b520946b3
Dell <Dell@DESKTOP-JRO69R2>
2023-10-26 08:10:00 +0300

    new v.2

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -1,5 +1,7 @@
 package com.example.task07;
 
+import java.nio.charset.UnmappableCharacterException;
+
 public class Task07Main {
 
     public static final String CHECKED = "checked";
@@ -12,13 +14,17 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
 
+        try {
+            processor.process();
         }
-        return null;
+        catch (RuntimeException e) {
+            return UNCHECKED;
+        }
+        catch (Exception e) {
+            return CHECKED;
+        }
+        return NONE;
     }
 
 }
