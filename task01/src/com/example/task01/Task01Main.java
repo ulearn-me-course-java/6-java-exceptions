fc861192a5514f839290b1cfdd6e432511b22a61
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2023-11-04 22:17:21 +0500

    Done

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,15 +1,12 @@
 package com.example.task01;
 
 public class Task01Main {
+    static StringBuilder builder;
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         codeWithNPE();
-         */
     }
 
     static void codeWithNPE() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        builder.append("a");
     }
 }
